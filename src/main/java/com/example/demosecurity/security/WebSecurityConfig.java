d9d1a16f1afaed26081f6adf7dbb4b7acf7204a3
Atsushi Sano <sano66@gmail.com>
2019-10-22 18:34:51 +0900

    ログアウト時にセッションを無効にし、クッキーを削除する #8

--- src/main/java/com/example/demosecurity/security/WebSecurityConfig.java
+++ src/main/java/com/example/demosecurity/security/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.demosecurity.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -16,4 +17,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		// リソースファイルを認証対象外とする issues/7
 		web.ignoring().antMatchers("/resources/**");
 	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// ログアウト時にセッションを無効にし、クッキーを削除する issues/8
+		super.configure(http);
+		http.logout().invalidateHttpSession(true).deleteCookies("JSESSIONID").permitAll();
+	}
+
 }
