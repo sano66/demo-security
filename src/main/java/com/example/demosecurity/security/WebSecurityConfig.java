6797169a01fa493612825ada62d40b5b854b0f7d
Atsushi Sano <sano66@gmail.com>
2019-10-22 18:32:10 +0900

    Spring Securityのデバッグ出力を有効にする #6

---
+++ src/main/java/com/example/demosecurity/security/WebSecurityConfig.java
@@ -0,0 +1,16 @@
+package com.example.demosecurity.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		// Spring Securityのデバッグ出力を有効にする issues/6
+		web.debug(true);
+	}
+}
